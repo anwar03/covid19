4d8f7045fe4d24dc0d26e327a42049344b45daee
anwar03 <anwarazim03@gmail.com>
2020-03-26 02:30:28 +0600

    Created Dashboard APIView and load dashboard template.

--- project_base/covid19/views.py
+++ project_base/covid19/views.py
@@ -1,12 +1,19 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer
+from django.db.models import Sum, Count
+from .models import Covid
 
 
 class Dashboard(APIView):
+    """ load dashboard view for Novel Coronavirus (COVID-19) Situation visual information."""
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'dashboard/dashboard.html'
 
     def get(self, request):
-        dashboard = 'Covid 19 Dashboard'
-        return Response({'dashboard': dashboard})
+        confirmed = Covid.objects.filter(created_at='2020-03-23').aggregate(Sum('confirmed'))['confirmed__sum']
+        death = Covid.objects.filter(created_at='2020-03-23').aggregate(Sum('death'))['death__sum']
+        recovered = Covid.objects.filter(created_at='2020-03-23').aggregate(Sum('recovered'))['recovered__sum']
+        countries = Covid.objects.values('country_id').distinct().count()
+
+        return Response({'confirmed': confirmed, 'death': death, 'recovered': recovered, 'countries': countries})
