9460e99b8b12bbe2e70bee3dc8bac9f762fc2e59
anwar03 <anwarazim03@gmail.com>
2020-03-28 15:25:44 +0600

    Created trend of new cases and implement barchart

--- project_base/covid19/urls/covid19.py
+++ project_base/covid19/urls/covid19.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('<int:pk>/', views.Covid19CountryDataUpdateApiView.as_view(), name=names.update_covid19_data),
     # store new information for country
     path('create/', views.Covid19CreateAPIView.as_view(), name=names.store_covie19_data),
+    path('newcase/', views.NewCaseListAPIView.as_view(), name=names.new_cases),
 ]
--- project_base/covid19/views.py
+++ project_base/covid19/views.py
@@ -7,7 +7,6 @@ from django.db.models import Sum
 from .models import Covid
 from .serializers import Covid19Serializer, CountryWiseSerializer
 
-
 class Dashboard(APIView):
     """ load dashboard view for Novel Coronavirus (COVID-19) Situation visual information."""
     renderer_classes = [TemplateHTMLRenderer]
@@ -62,3 +61,25 @@ class CountryWiseListAPIView( ListAPIView ):
         # filter covid19 information for specific day
         queryset = Covid.objects.filter(created_at=day)
         return queryset
+
+
+class NewCaseListAPIView( APIView ):
+    permission_classes = (AllowAny, )
+
+    def get(self, request):
+        previous_value = 0
+        new_cases = list()
+        # get country from query params
+        country = self.request.query_params.get('country', None)
+        queryset = Covid.objects.all()
+        if country is not None:
+            queryset = queryset.filter(country__name__iexact=country)
+
+        queryset = queryset.values('created_at').order_by('created_at').annotate(confirmed=Sum('confirmed'))
+
+        for item in queryset:
+            new_cases.append({ 'created_at': item['created_at'], 'confirmed': item['confirmed'] - previous_value})
+            previous_value = item['confirmed']
+
+        return Response(new_cases)
+
