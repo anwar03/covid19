2a238ce5dc15cd13bc942031d84585eca427f438
anwar03 <anwarazim03@gmail.com>
2020-04-15 21:27:26 +0600

    Changed the dashboard API query for performance optimization.

---
+++ project_base/covid19/debugger/decorators.py
@@ -0,0 +1,26 @@
+from django.db import connection, reset_queries
+import time
+import functools
+
+def query_debugger(func):
+
+    @functools.wraps(func)
+    def inner_func(*args, **kwargs):
+
+        reset_queries()
+
+        start_queries = len(connection.queries)
+
+
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(connection.queries)
+        end_queries = len(connection.queries)
+
+        print(f"Function : {func.__name__}")
+        print(f"Number of Queries : {end_queries - start_queries}")
+        print(f"Finished in : {(end - start):.4f}s")
+        return result
+
+    return inner_func
--- project_base/covid19/views/dashboard.py
+++ project_base/covid19/views/dashboard.py
@@ -2,8 +2,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.permissions import AllowAny
-from django.db.models import Sum
+from django.db.models import Sum, Count
 from ..models import Covid
+
+from ..debugger.decorators import query_debugger
+
 
 class Dashboard(APIView):
     """ load dashboard view for Novel Coronavirus (COVID-19) Situation visual information."""
@@ -19,6 +22,7 @@ class Dashboard(APIView):
 class DashboardInfo(APIView):
     """ load dashboard view for Novel Coronavirus (COVID-19) Situation visual information."""
 
+    @query_debugger
     def get(self, request):
         # cause of filtering specific day I have not updated data after 2020-03-23
         day = '2020-03-23'
@@ -29,14 +33,24 @@ class DashboardInfo(APIView):
             queryset = queryset.filter(country__name__iexact=country)
         # filter covid19 information for specific day
         queryset = queryset.filter(created_at=day)
+
+        """
+        section 1 
+        # deprecated this section because of this section hit 2 times in database 
+        
         # get total confirmed, death and recovered information from queryset
-        dashboard = queryset.aggregate(Sum('confirmed'), Sum('death'), Sum('recovered'))
+        dashboard = queryset.aggregate(confirmed = Sum('confirmed'), death=  Sum('death'),recovered = Sum('recovered'))
         # count distinct country from queryset
         countries_count = queryset.values('country_id').distinct().count()
+        """
 
-        confirmed = 0 if dashboard['confirmed__sum'] is None else dashboard['confirmed__sum']
-        death = 0 if dashboard['death__sum'] is None else dashboard['death__sum']
-        recovered = 0 if dashboard['recovered__sum'] is None else dashboard['recovered__sum']
-        countries = 0 if countries_count is None else countries_count
+        # section 2 using this section because of this query only 1 time hit in database
+        dashboard = queryset.aggregate(confirmed = Sum('confirmed'),death= Sum('death'),recovered = Sum('recovered'), countries = Count('country_id'))
+
+        confirmed = 0 if dashboard['confirmed'] is None else dashboard['confirmed']
+        death = 0 if dashboard['death'] is None else dashboard['death']
+        recovered = 0 if dashboard['recovered'] is None else dashboard['recovered']
+        countries = 0 if dashboard['countries'] is None else dashboard['countries']
+        # countries = 0 if countries_count is None else countries_count
 
         return Response({'confirmed': confirmed, 'death': death, 'recovered': recovered, 'countries': countries})
